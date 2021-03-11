fe13f2d1060bbbb525d3c27845a33f5c3f7cad76
Lewis Daly <lewisdaly@me.com>
2021-03-11 22:10:21 +1030

    feat: adding in ability to clone private repo

--- infra/playbooks/deploy_app.sh
+++ infra/playbooks/deploy_app.sh
@@ -4,9 +4,10 @@ cd /home/ubuntu
 rm -rf /home/ubuntu/app
 mkdir -p /home/ubuntu/app
 
+ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 # note: I added a deploy key here: https://github.com/mojaloop/ml-iso-hackathon/settings/keys
 # in order to get this to work for a private repo
 git clone git@github.com:mojaloop/ml-iso-hackathon.git app
 cd app
 
-sudo docker-compose up -d
+docker-compose up -d traefik landing-page
